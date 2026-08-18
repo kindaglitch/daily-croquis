7f7e78b9a68401a94040267764a93e5c6f4cda68
kindaglitch <kindansasayaki@gmail.com>
2026-08-18 23:21:34 +0900

    feat: improve layout and feedback comment

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='daily-croquis-v4';
+const CACHE='daily-croquis-v6';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(
